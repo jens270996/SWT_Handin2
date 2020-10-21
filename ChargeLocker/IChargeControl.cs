407a9fda75236f18eb96478050500aefbb308868
Joachim <Kroyer.joachim@gmail.com>
2020-10-21 12:54:43 +0200

    tilføjet skelet til IChargeControl og ChargeControl Tilføjet IUSBCharger og USBCharger Simulator

--- ChargeLocker/IChargeControl.cs
+++ ChargeLocker/IChargeControl.cs
@@ -2,6 +2,8 @@
 {
     public interface IChargeControl
     {
-        
+        void startCharge();
+        void StopCharge();
+        bool IsConnected();
     }
 }
--- ChargeLocker/IUSBCharger.cs
+++ ChargeLocker/IUSBCharger.cs
@@ -1,7 +1,27 @@
-﻿namespace ChargeLocker
+﻿using System;
+
+namespace ChargeLocker
 {
+    public class CurrentEventArgs : EventArgs
+    {
+        // Value in mA (milliAmpere)
+        public double Current { set; get; }
+    }
+
     public interface IUSBCharger
     {
-        
+        // Event triggered on new current value
+        event EventHandler<CurrentEventArgs> CurrentValueEvent;
+
+        // Direct access to the current current value
+        double CurrentValue { get; }
+
+        // Require connection status of the phone
+        bool Connected { get; }
+
+        // Start charging
+        void StartCharge();
+        // Stop charging
+        void StopCharge();
     }
 }
