3174a7280dc3662b3e0c748338720e4a7002b233
Jens Kristensen <jens-nk@hotmail.com>
2020-10-21 14:41:46 +0200

    Created StationControl class

---
+++ ChargeLocker/StationControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChargeLocker
+{
+    
+
+    public class StationControl
+    {
+        private IDoor door;
+        private IRfidReader rfidReader;
+        private IMessageFormatter messageFormatter;
+        private IChargeControl chargeControl;
+        private ILogFile logFile;
+        private int rfid;
+
+        public  StationControl(IDoor Door,IRfidReader RFIDReader,)
+    }
+}
