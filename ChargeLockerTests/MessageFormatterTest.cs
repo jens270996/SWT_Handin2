f24d5caa170251aef3c6336e256b57e563cedd9a
Jens Kristensen <jens-nk@hotmail.com>
2020-10-22 13:15:15 +0200

    Messageformater test

--- ChargeLockerTests/MessageFormatterTest.cs
+++ ChargeLockerTests/MessageFormatterTest.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ChargeLocker;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace ChargeLockerTests
 {
     class MessageFormatterTest
     {
+        private IMessageFormatter uut_;
+        private IDisplayer displayer;
+        [SetUp]
+        public void Setup()
+        {
+            displayer = Substitute.For<IDisplayer>();
+            uut_=new MessageFormatter(displayer);
+        }
     }
 }
